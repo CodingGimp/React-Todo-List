535c1813ca442581407a2865b91116a77c07ee1e
Mario Jamid <coding.gimp@gmail.com>
2019-08-08 13:59:11 -0700

    Todo component

---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Todos extends Component {
+    render() {
+        return this.props.todos.map(todo => (
+            <h3>{todo.title}</h3>
+        ));
+    }
+}
+
