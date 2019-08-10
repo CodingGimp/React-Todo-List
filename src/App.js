90d0fde1dec6cd3e3e1cb5c077aa429fc959c634
Mario Jamid <coding.gimp@gmail.com>
2019-08-10 14:20:14 -0700

    Props

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
-import React, { Component } from 'react';
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
+import React, { Component } from 'react'
+import Todos from './components/Todos'
 
-import './App.css'
+export default class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, officiis.',
+                completed: false,
+            },
+            {
+                id: 2,
+                title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+                completed: false,
+            },
+            {
+                id: 3,
+                title: 'Lorem ipsum dolor sit amet.',
+                completed: false,
+            },
+        ]
+    }
 
-class App extends Component {
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Take out the trash.',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Make dinner.',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Clean the house.',
-        completed: false
-      },
-    ]
-  }
-
-  delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
-  }
-
-  markComplete = (id) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container'>
+                <Todos todos={this.state.todos} />
+            </div>
+        )
+    }
 }
-
-export default App;
