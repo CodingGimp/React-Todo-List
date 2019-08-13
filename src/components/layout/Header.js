dacaaa35788561360d84b3c55bc674ff573c46c8
Mario Jamid <coding.gimp@gmail.com>
2019-08-13 16:34:19 -0700

    Nav Links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header className='text-center mb-3 bg-dark text-light' >
             <h1>Todo List</h1>
+            <Link className='page-item active' to='/'>Home</Link> | <Link className='text-light' to='/about'>About</Link>
         </header>
     )
 }
